ecfb3b1f63328af21dbe9d232109837bf398f580
0rangorang <102425287+0rangorang@users.noreply.github.com>
2026-03-06 21:23:44 +0700

    Delete Sbi.RoomDisplay directory
    Deleting for v3 place

--- Sbi.RoomDisplay/Helpers/BrandMode.cs
+++
@@ -1,9 +0,0 @@
-// Mode background untuk menentukan logo mana yang dipakai
-// Tidak boleh pakai string bebas (hindari typo)
-
-public enum BrandMode
-{
-    Light,     // Background putih
-    Dark,      // Background hitam/navy
-    Accent     // Background merah corporate
-}
