7327f2691e8378f64a54ed6a186e0e8f374f4a94
0rangorang <102425287+0rangorang@users.noreply.github.com>
2026-03-06 20:34:40 +0700

    v2 stable tv dashboard w/ MockApi&custom UI Layout

---
+++ Sbi.RoomDisplay/Helpers/BrandMode.cs
@@ -0,0 +1,9 @@
+// Mode background untuk menentukan logo mana yang dipakai
+// Tidak boleh pakai string bebas (hindari typo)
+
+public enum BrandMode
+{
+    Light,     // Background putih
+    Dark,      // Background hitam/navy
+    Accent     // Background merah corporate
+}
