d54bdc8cbe627b6a4dd14479558b96c3440cf8bd
0rangorang <102425287+0rangorang@users.noreply.github.com>
2026-03-06 20:55:36 +0700

    Delete MeetingRoomDashboard directory
    I thought "Target a Commit" in "Releases" is only the modified file. It turns out, the main branch at that "Commit" time. So, there's no need to have this v1 in main, when this v1 already have its "Releases".

--- MeetingRoomDashboard/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MeetingRoomDashboard.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
